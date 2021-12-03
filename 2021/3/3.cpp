3000a65f06b486309f5a433f1bf6abe1e73ba487
Søren Dyhr <siadyhr@gmail.com>
2021-12-03 09:04:52 +0100

    Ready part1 for part2 (vectors)
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- 2021/3/3.cpp
+++ 2021/3/3.cpp
@@ -2,10 +2,11 @@
 #include <string>
 #include <cmath>
 #include <typeinfo>
+#include <vector>
 
-const int N = 12;
+const int N = 1000;
 
-int part1(std::string diagnostics[], int width, int length) {
+std::vector<int> part1(std::string diagnostics[], int width, int length) {
 	int ones[width];
 	for (int i = 0; i < width; ++i) {
 		ones[i] = 0;
@@ -18,20 +19,42 @@ int part1(std::string diagnostics[], int width, int length) {
 			}
 		}
 	}
-	int gamma = 0;
-	int epsilon = 0;
+	std::vector<int> result;
 	for (int i = 0; i < width; ++i) {
 		if (ones[i] > N/2) {
-			gamma += std::pow(2, width-i-1);
+			result.push_back(1);
 		} else {
-			epsilon += std::pow(2, width-i-1);
+			result.push_back(0);
 		}
 	}
 	for (int i = 0; i < width; ++i) {
 		std::cerr << ones[i] << " ";
 	}
 	std::cerr << "\n";
-	return gamma * epsilon;
+	return result;
+}
+
+int vec2int(std::vector<int> vector) {
+	int result = 0;
+	int i = 0;
+	for (auto & x : vector) {
+		std::cerr << i << " " << x << "\n";
+		result += x * std::pow(2, vector.size() - i - 1);
+		++i;
+	}
+	return result;
+}
+
+std::vector<int> twos_complement(std::vector<int> vector) {
+	std::vector<int> result;
+	for (auto & x : vector) {
+		if (x == 1) {
+			result.push_back(0);
+		} else {
+			result.push_back(1);
+		}
+	}
+	return result;
 }
 
 int main() {
@@ -45,7 +68,10 @@ int main() {
 		std::cin >> diagnostics[i];
 	}
 
-	std::cout << part1(diagnostics, width, N) << "\n";
+	std::vector<int> most_common = part1(diagnostics, width, N);
+
+	// Part 1
+	std::cout << vec2int(twos_complement(most_common)) * vec2int(most_common) << "\n";
 
 	return 1;
 }
