7e083f80ddf50fc49771b9c6da5b04d87130bf65
Søren Dyhr <siadyhr@gmail.com>
2021-12-06 09:57:57 +0100

    Refactor to add support for D6P2
    Signed-off-by: Søren Dyhr <siadyhr@gmail.com>

--- 2021/6/6.py
+++ 2021/6/6.py
@@ -1,19 +1,18 @@
-n_days = 80
-base_length = 6
-fishes = [0]*(n_days + base_length)
+def fish_after_days(fishes_in, n_days):
+    fishes = [x for x in fishes_in]
+    new_fish = 0
+    for _ in range(n_days):
+        new_fish = fishes[0]
+        for i, fish in enumerate(fishes[1:], 1):
+            fishes[i-1] = fish
+        fishes[8] += new_fish
+        fishes[6] += new_fish
+    return sum(fishes)
 
+fishes = [0]*(256 + 6)
 for x in [int(y) for y in input().split(',')]:
     fishes[x] += 1
 
-new_fish = 0
-for _ in range(n_days):
-#    print(sum(fishes))
-#    print(fishes)
-    new_fish = fishes[0]
-    for i, fish in enumerate(fishes[1:], 1):
-        fishes[i-1] = fish
-    fishes[8] += new_fish
-    fishes[6] += new_fish
-
-print(fishes)
-print(sum(fishes))
+print(fish_after_days(fishes, 18))
+print(fish_after_days(fishes, 80))
+print(fish_after_days(fishes, 256))
