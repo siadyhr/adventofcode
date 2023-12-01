102c0569d3c52bf8608f6fbf3133539896e2ba30
Søren István Adorján Dyhr <siadyhr@gmail.com>
2023-12-01 20:52:56 +0100

    Both parts working but the code is terrible
    Signed-off-by: Søren István Adorján Dyhr <siadyhr@gmail.com>

--- 2023/01.hs
+++ 2023/01.hs
@@ -1,7 +1,9 @@
 module Main
     where
 
-import System.IO (isEOF)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromJust, isJust)
 
 isInteger :: Char -> Bool
 isInteger x = isInList x "0123456789"
@@ -20,7 +22,28 @@ readLines = fmap lines . readFile
 
 part1 l = sum( map firstAndLastInt l )
 
-main = do
+wordToIntList = zip ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"] [0..9]
+wordToInt = Map.fromList wordToIntList
+reverseWordToInt = Map.fromList [(reverse x, y) | (x, y) <- wordToIntList]
 
+getFirstInteger :: String -> Map String Int -> Int
+getFirstInteger (x:_) _
+    | isInteger x = (read [x])
+getFirstInteger x dict
+    | Map.member (take 3 x) dict = fromJust (Map.lookup (take 3 x) dict)
+    | Map.member (take 4 x) dict = fromJust (Map.lookup (take 4 x) dict)
+    | Map.member (take 5 x) dict = fromJust (Map.lookup (take 5 x) dict)
+getFirstInteger x dict = getFirstInteger (tail x) dict
+
+getLastInteger :: String -> Int
+getLastInteger x = getFirstInteger(reverse x) reverseWordToInt
+
+getFirstAndLastIntegerPart2 x = read ( show (getFirstInteger x wordToInt) ++ show (getLastInteger x) ) :: Int
+
+part2 l = sum ( map getFirstAndLastIntegerPart2 l )
+
+main = do
     rawInput <- (readLines "01.in")
     putStrLn (show (part1 rawInput))
+    putStrLn (show (part2 rawInput))
+    putStrLn (show (getFirstAndLastIntegerPart2 (rawInput !! 1)))
