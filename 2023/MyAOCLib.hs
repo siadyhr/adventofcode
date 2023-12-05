b769b695a3bfa1ce7f40ad911fe0c7f52ed0b926
Søren István Adorján Dyhr <siadyhr@gmail.com>
2023-12-05 18:29:45 +0100

    Let my split function work on any (Eq a) => [a]
    Signed-off-by: Søren István Adorján Dyhr <siadyhr@gmail.com>

--- 2023/MyAOCLib.hs
+++ 2023/MyAOCLib.hs
@@ -3,14 +3,14 @@ module MyAOCLib where
 readLines :: FilePath -> IO [String]
 readLines = fmap lines . readFile
 
-splitRaw :: String -> Char -> String -> [String]
-splitRaw "" c partialWord = [partialWord]
+splitRaw :: (Eq a) => [a] -> a -> [a] -> [[a]]
+splitRaw [] c partialWord = [partialWord]
 splitRaw (x:s) c partialWord
-    | x == c    = [partialWord] ++ splitRaw s c ""
+    | x == c    = [partialWord] ++ splitRaw s c []
     | otherwise = splitRaw s c (partialWord ++ [x])
 
-split :: Char -> String -> [String]
-split c s = splitRaw s c ""
+split :: (Eq a) => a -> [a] -> [[a]]
+split c s = splitRaw s c []
 
 lstrip :: Char -> String -> String
 lstrip c (x:s)
