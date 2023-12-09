db39aa41a2a0a96d6a0ae66cf4e25398603d5887
Søren István Adorján Dyhr <siadyhr@gmail.com>
2023-12-09 19:15:28 +0100

    Add some exploration functions
    Signed-off-by: Søren István Adorján Dyhr <siadyhr@gmail.com>

--- 2023/08.hs
+++ 2023/08.hs
@@ -65,22 +65,69 @@ findSuffixAndCountRaw n tree startLabels endSuffix (direction:directions)
 findSuffixAndCount :: Map String (String, String) -> [String] -> Char -> String -> Int
 findSuffixAndCount = findSuffixAndCountRaw 0
 
+findNextSuffixAndCountRaw :: Int -> Map String (String, String) -> String -> Char -> String -> (String, Int)
+findNextSuffixAndCountRaw n tree startLabel endSuffix (direction:directions)
+    | ((startLabel!!2) == endSuffix) && (n /= 0)
+        = (startLabel, n)
+    | otherwise 
+        = findNextSuffixAndCountRaw (n+1) tree nextLabel endSuffix directions
+    where
+        nextLabel
+            | direction == 'L'  = leftLabel
+            | otherwise         = rightLabel
+        (leftLabel, rightLabel) = defaultLookup startLabel ("0", "0") tree
+
+
+findNextSuffixAndCount :: Map String (String, String) -> String -> Char -> String -> (String, Int)
+findNextSuffixAndCount = findNextSuffixAndCountRaw 0
+
 findLabelsWithSuffix :: Char -> Map String (String, String) -> [String]
 findLabelsWithSuffix suffix tree = filter (\x -> (x!!2 == suffix)) (Map.keys tree)
+
+getFirstMatching :: Char -> Map String (String, String) -> [String] -> String -> [(String, Int)]
+getFirstMatching suffix tree startLabels directions = map (\x -> findNextSuffixAndCount tree x suffix directions) startLabels
+
+---
+
+navigate :: (String, Map String (String, String)) -> String -> Int -> [(Int, String)]
+navigate (direction:directions, tree) startLabel step
+    | (startLabel!!2) == 'Z'    = (step, startLabel):continueNavigating
+    | otherwise                 = continueNavigating
+    where
+        continueNavigating = navigate (directions, tree) nextLabel (step + 1)
+        nextLabel
+            | direction == 'L'  = leftLabel
+            | otherwise         = rightLabel
+        (leftLabel, rightLabel) = defaultLookup startLabel ("0", "0") tree
+
+test :: (String, Map String (String, String)) -> Int -> [(Int, String)]
+test (directions, tree) nDirections = output
+    where
+        output = map (\(x, y) -> (x `div` nDirections, y)) outputRaw
+        outputRaw = take 10 $ navigate (directions, tree) startLabel 0
+        startLabel = startLabels!!1
+        startLabels = findLabelsWithSuffix 'A' tree
 
 part1 :: [String] -> Int
 part1 s = findAndCount tree "AAA" "ZZZ" directions
     where
         (directions, tree) = parse s
 
-part2 :: [String] -> Int
-part2 s = findSuffixAndCount tree startLabels 'Z' directions
+part2old :: [String] -> Int
+part2old s = findSuffixAndCount tree startLabels 'Z' directions
     where
         (directions, tree) = parse s
         startLabels = findLabelsWithSuffix 'A' tree
 
+part2 :: [String] -> [(String, Int)]
+part2 s = getFirstMatching 'Z' tree startLabels directions
+    where
+        startLabels = findLabelsWithSuffix 'A' tree
+        (directions, tree) = parse s
+
 main = do
     rawInput <- (readLines "08.in")
     putStrLn "Hello, World!"
-    putStrLn $ show $ part1 rawInput
-    putStrLn $ show $ part2 rawInput
+--    putStrLn $ show $ part1 rawInput
+--    putStrLn $ show $ part2 rawInput
+    putStrLn $ show $ test (parse rawInput) (length (rawInput!!0))
