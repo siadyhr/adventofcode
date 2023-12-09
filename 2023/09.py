1ec5e9eaa9592e420ebf8727efd7a7527741c710
Søren István Adorján Dyhr <siadyhr@gmail.com>
2023-12-09 12:02:03 +0100

    Describe algorithm
    Signed-off-by: Søren István Adorján Dyhr <siadyhr@gmail.com>

--- 2023/09.py
+++ 2023/09.py
@@ -1,6 +1,12 @@
 import sys
 
 def extrapolateNext(L):
+    # iteratively constract the derivative
+    # of the previous run, overwriting the
+    # array except for the last
+    # `iteration - ` entries, which exactly
+    # are the last entries we need to add
+    # up in the end.
     for iteration in range(len(L)):
         for i in range(len(L) - iteration - 1):
             L[i] = L[i+1] - L[i]
@@ -10,6 +16,8 @@ result1 = 0
 result2 = 0
 for rawIn in sys.stdin:
     parsed = [int(x) for x in rawIn.split(" ")]
+    # extrapolateNext is desctructive, so if we want
+    # to reuse the `parsed` array we have to copy it
     result1 += extrapolateNext([x for x in parsed])
     result2 += extrapolateNext([x for x in parsed[::-1]])
 
