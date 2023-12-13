5e24ed344fdc79d2c6d7a4935ac8b4dc9a6f7587
Søren István Adorján Dyhr <siadyhr@gmail.com>
2023-12-13 16:01:01 +0100

    Add some functions; get indices of elements, sort, and transpose
    Signed-off-by: Søren István Adorján Dyhr <siadyhr@gmail.com>

--- 2023/MyAOCLib.hs
+++ 2023/MyAOCLib.hs
@@ -16,3 +16,35 @@ lstrip :: Char -> String -> String
 lstrip c (x:s)
     | c == x    = lstrip c s
     | otherwise = x:s
+
+-- TODO: Use `Maybe`
+getFirstIndex :: (Eq a) => a -> [a] -> Int
+getFirstIndex _ [] = error "No occurence"
+getFirstIndex y (x:xs)
+    | x == y    = 0
+    | otherwise = 1 + getFirstIndex y xs
+
+
+getIndices :: (Eq a) => a -> [a] -> [Int]
+getIndices = getIndices' 0
+    where
+        getIndices' :: (Eq a) => Int -> a -> [a] -> [Int]
+        getIndices' _ _ [] = []
+        getIndices' index symbol (x:xs)
+            | x == symbol   = index:rest
+            | otherwise     = rest
+            where rest = getIndices' (index+1) symbol xs
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [] = []
+quickSort (x:xs) = smallerSorted ++ (x:largerSorted)
+    where
+        smallerSorted = quickSort $ filter (<=x) xs
+        largerSorted = quickSort $ filter (>x) xs
+
+transpose :: [[a]] -> [[a]]
+transpose [] = []
+transpose matrix
+    | (length $ head matrix) == 1   = [[x | [x] <- matrix]]
+    | otherwise = (map head matrix):(transpose $ map tail matrix)
+
